8da902600b244f69e6cc08391ca477afef4a2765
Naitikjain29 <naitikj2912@gmail.com>
2025-07-12 14:07:31 +0530

    hello

--- backend/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { registerUser, createUser } = require("../controllers/userController");
-
-router.post("/register", registerUser);
-router.post("/", createUser);
-
-module.exports = router;
